891f14d66d4e98b74d9224d77e8004860a132803
progmamun <mamun333089@gmail.com>
2023-07-10 11:10:26 +0600

    login-done

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -68,7 +68,7 @@ export default function Products() {
       </div>
       <div className="col-span-9 grid grid-cols-3 gap-10 pb-20">
         {productsData?.map((product: IProduct) => (
-          <ProductCard product={product} key={product.name} />
+          <ProductCard product={product} />
         ))}
       </div>
     </div>
