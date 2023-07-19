c858e88a30a825da71ae32b80644efd0e84d1d82
progmamun <mamun333089@gmail.com>
2023-07-19 10:53:02 +0600

    [fixed-navigate-issue]

--- src/routes/privateRoute.tsx
+++ src/routes/privateRoute.tsx
@@ -9,14 +9,14 @@ interface IProps {
 export default function PrivateRoute({ children }: IProps) {
   const { user, isLoading } = useAppSelector((state) => state.user);
 
-  const { pathname } = useLocation();
+  const location = useLocation();
 
   if (isLoading) {
     return <p>Loading...</p>;
   }
 
   if (!user.email && !isLoading) {
-    return <Navigate to="/login" state={{ path: pathname }}></Navigate>;
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
   }
   return children;
 }
